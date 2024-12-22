6e9286eb13563a3580a30de9fb0a84505bc345b7
Abanob Essam <abanobessam19@gmail.com>
2024-12-22 17:41:26 +0200

    small issue fixed

--- src/main/java/com/example/lms/controller/AttendanceData.java
+++
@@ -1,8 +0,0 @@
-package com.example.lms.controller;
-import lombok.Data;
-
-@Data
-public class AttendanceData {
-    private int studentId;
-    private int otp;
-}
--- src/main/java/com/example/lms/controller/NotificationData.java
+++
@@ -1,6 +0,0 @@
-package com.example.lms.controller;
-
-public class NotificationData {
-    int userId;
-    String message;
-}
