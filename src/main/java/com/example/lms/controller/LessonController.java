1a258d1f717117999fc55f898662bbb4b2d445a5
Mariam Ayman <121129576+mariam511@users.noreply.github.com>
2024-12-23 20:01:40 +0200

    Update lessons

--- src/main/java/com/example/lms/controller/LessonController.java
+++ src/main/java/com/example/lms/controller/LessonController.java
@@ -4,6 +4,7 @@ import com.example.lms.dto.AttendanceData;
 import com.example.lms.model.course_related.Lesson;
 import com.example.lms.service.LessonService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,16 +16,19 @@ public class LessonController {
     private final LessonService lessonService;
 
     @PostMapping("/{courseId}/add")
+    @PreAuthorize("hasAnyAuthority('INSTRUCTOR')")
     public Lesson addLessonToCourse(@RequestBody Lesson lesson, @PathVariable int courseId) {
         return lessonService.addLesson(lesson, courseId);
     }
 
     @GetMapping("/{courseId}/show_lessons")
+    @PreAuthorize("hasAnyAuthority('INSTRUCTOR', 'ADMIN', 'STUDENT')")
     public List<Lesson> getLessonsForCourse(@PathVariable int courseId) {
         return lessonService.getLessonsForCourse(courseId);
     }
 
     @PostMapping("/{lessonId}/attend")
+    @PreAuthorize("hasAnyAuthority('STUDENT')")
     public String attendLesson(
             @PathVariable int lessonId,
             @RequestBody AttendanceData attendanceData) {
